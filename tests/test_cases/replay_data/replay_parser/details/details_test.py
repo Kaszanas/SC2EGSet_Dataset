1f90b81dbd61147bcd71dc34386a09a83cb12683
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-07-27 16:48:53 +0200

    Testing methods in details_test.py

--- tests/test_cases/replay_data/replay_parser/details/details_test.py
+++ tests/test_cases/replay_data/replay_parser/details/details_test.py
@@ -1,3 +1,6 @@
+import unittest
+
+from sc2egset_dataset.dataset.replay_data.replay_parser.details.details import Details
 """
     **Incorrect Usage Examples:**
 
@@ -10,3 +13,27 @@
     ...
     KeyError: 'gameSpeed'
 """
+class TestDetails(unittest.TestCase):
+
+    def test_from_dict_correct(self):
+        #given
+        details_dict = {"gameSpeed": "Faster",
+                        "isBlizzardMap": True,
+                        "timeUTC": "2017-04-29T05:15:32.4903483+02:00"
+                        }
+        #when
+        details_object = Details.from_dict(d=details_dict)
+        # then
+        self.assertTrue(details_object)
+
+    def test_from_dict_incorrect_parameter(self):
+        #given
+        details_dict = {
+                        "gameSpeed": "Fasterrr",
+                        "isBlizzardMap": True,
+                        "timeUTC": "2017-04-29T05:15:32.4903483+02:00"
+                        }
+        #when
+        details_object = Details.from_dict(d=details_dict)
+        #then
+        self.assertRaises(TypeError,details_object)
