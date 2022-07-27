285c0a0f9928f5a9bf1c29153b091c9a743b5d76
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-07-27 19:34:26 +0200

    test_directory_not_exist_exception and test_url_not_set_exception methods

--- tests/test_cases/pytorch_datasets/replaypack_dataset_test.py
+++ tests/test_cases/pytorch_datasets/replaypack_dataset_test.py
@@ -9,6 +9,7 @@ from sc2egset_dataset.dataset.pytorch_datasets.sc2_replaypack_dataset import (
 from sc2egset_dataset.dataset.replay_data.sc2_replay_data import SC2ReplayData
 from sc2egset_dataset.dataset.utils.zip_utils import unpack_zipfile
 
+from sc2egset_dataset.dataset.utils.download_utils import download_replaypack
 from tests.settings_test import TEST_REPLAYPACKS
 from tests.test_utils.test_utils import get_specific_asset, get_test_output_dir
 
@@ -30,7 +31,6 @@ class SC2ReplaypackDatasetTest(unittest.TestCase):
         # Initializing the unpacked where it should be:
         cls.unpacked = Path(cls.unpack_dir_path, cls.test_replaypack_name)
         cls.download = Path(cls.download_dir_path, cls.test_replaypack_name)
-
         # If it doesn't exist, unpack the test .zip archive:
         if not cls.unpacked.exists():
             # Unpacks the replaypack that will be used for testing:
@@ -92,3 +92,22 @@ class SC2ReplaypackDatasetTest(unittest.TestCase):
         self.assertNotEqual(len(sc2_replaypack_dataset), 0)
         # It is possible to retrieve a single file by index:
         self.assertIsInstance(sc2_replaypack_dataset[0], SC2ReplayData)
+
+    def test_directory_not_exist_exception(self):
+        with self.assertRaises(Exception) as e:
+            sc2_replaypack_dataset = SC2ReplaypackDataset(
+                replaypack_name=self.test_replaypack_name,
+                unpack_dir="wrong",
+                download_dir=self.download_dir_path
+            )
+        self.assertEqual(str(e.exception),"Replaypack unpack directory does not exist!")
+
+    def test_url_not_set_exception(self):
+        with self.assertRaises(Exception) as e:
+            sc2_replaypack_dataset = SC2ReplaypackDataset(
+                replaypack_name=TEST_REPLAYPACKS[0][0],
+                unpack_dir=self.unpack_dir_path,
+                download_dir=self.download_dir_path,
+                download=True
+            )
+        self.assertEqual(str(e.exception),"Detected empty URL! Cannot download a replaypack!")
